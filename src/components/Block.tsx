e21eb5cc8fb26cbc0ee052a8cf0a8b985e466bac
Fernando Ribas <fribas84@gmail.com>
2023-06-15 17:43:16 -0300

    styling and poc done.

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,122 +1,131 @@
 import { useState } from 'react';
-import {Form} from 'react-router-dom';
+import { Form } from 'react-router-dom';
 
 type BlockProps = {
-    number?: number,
+    height?: number,
     nonce?: number,
     data?: string,
     prevHash?: string,
     hash?: string
 }
 
-const Block = ({number,
-                nonce,
-                data,
-                prevHash,
-                hash,}:BlockProps) => {
-    
-    const [attempt,setAttempt] = useState<number>(0);
-  
+const Block = ({ height,
+    nonce,
+    data,
+    prevHash,
+    hash, }: BlockProps) => {
 
+    const [attempt, setAttempt] = useState<number>(0);
+
+    console.log("height: ", height);
     return (
-    <div>
-        <div className='bg-white shadow rounded-sm md:w-3/4 mx-auto px-5 py-10 mt-20bg-white shadow rounded-sm md:w-3/4 mx-auto px-5 py-10 mt-20'>
-        <div className="mb-4">
-                <label
-                    className="text-gray-800"
-                    htmlFor="number"
-                >Block Number:</label>
-                <input 
-                    id="number"
-                    type="text"
-                    className="mt-2 block w-full p-3 bg-gray-50"
-                    placeholder="Customer Name"
-                    name="block Number"
-                    defaultValue={number}
-                />
-            </div>
-            <div className="mb-4">
-                <label
-                    className="text-gray-800"
-                    htmlFor="nonce"
-                >Nonce:</label>
-                <input 
-                    id="nonce"
-                    type="text"
-                    className="mt-2 block w-full p-3 bg-gray-50"
-                    placeholder="nonce"
-                    name="nonce"
-                    defaultValue={nonce}
-                    disabled={true}
-                />
+        <div>
+            <div className='bg-white shadow rounded-sm md:w-3/4 mx-auto px-5 py-10 mt-20bg-white shadow rounded-sm md:w-3/4 mx-auto px-5 py-10 mt-20'>
+                <div className="mb-4">
+                    <label
+                        className="text-gray-800"
+                        htmlFor="height"
+                    >Block Height:</label>
+                    <input
+                        id="height"
+                        type="number"
+                        className="mt-2 block w-full p-3 bg-gray-50"
+                        placeholder="Block Height"
+                        name="height"
+                        defaultValue={height}
+                        disabled={true}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label
+                        className="text-gray-800"
+                        htmlFor="nonce"
+                    >Nonce:</label>
+                    <input
+                        id="nonce"
+                        type="text"
+                        className="mt-2 block w-full p-3 bg-gray-50"
+                        placeholder="nonce"
+                        name="nonce"
+                        defaultValue={nonce}
+                        disabled={true}
+                    />
+                </div>
+
+                <div className="mb-4">
+                    <label
+                        className="text-gray-800"
+                        htmlFor="data"
+                    >Data:</label>
+                    <textarea
+                        id="data"
+                        className="mt-2 block w-full p-3 bg-gray-50"
+                        placeholder="data"
+                        name="data"
+                        defaultValue={data}
+                    />
+                </div>
+
+                <div className="mb-4">
+                    <label
+                        className="text-gray-800"
+                        htmlFor="prevHash"
+                    >Previous Hash:</label>
+                    <input
+                        id="prevHash"
+                        type="text"
+                        className="mt-2 block w-full p-3 bg-gray-50"
+                        placeholder="prevHash"
+                        name="prevHash"
+                        defaultValue={prevHash}
+                        disabled={true}
+                    />
+
+                </div>
+                <div className="mb-4">
+                    <label
+                        className="text-gray-800"
+                        htmlFor="hash"
+                    >Hash:</label>
+
+                    <input
+                        id="hash"
+                        type="text"
+                        className="mt-2 block w-full p-3 bg-gray-50"
+                        placeholder="hash"
+                        name="hash"
+                        defaultValue={hash}
+                        disabled={true}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label
+                        className="text-gray-800"
+                        htmlFor="attenpt"
+                    >Attempt:</label>
+
+                    <input
+                        id="attempt"
+                        type="number"
+                        className="mt-2 block w-full p-3 bg-gray-50"
+                        placeholder="attempt"
+                        name="attempt"
+                        defaultValue={attempt}
+                        disabled={true}
+                    />
+                </div>
+                <div className=''>
+                <button
+                    className='w-full bg-teal-600 text-white px-3 py-1 font-bold rounded-sm hover:bg-teal-300'
+                    >
+                    Mine
+                </button>
+                </div>
             </div>
 
-            <div className="mb-4">
-                <label
-                    className="text-gray-800"
-                    htmlFor="data"
-                >Data:</label>
-                <textarea 
-                    id="data"
-                    className="mt-2 block w-full p-3 bg-gray-50"
-                    placeholder="data"
-                    name="data"
-                    defaultValue={data}
-                />
-            </div>
-           
-            <div className="mb-4">
-                <label
-                    className="text-gray-800"
-                    htmlFor="prevHash"
-                >Previous Hash:</label>
-                <input 
-                    id="prevHash"
-                    type="text"
-                    className="mt-2 block w-full p-3 bg-gray-50"
-                    placeholder="prevHash"
-                    name="prevHash"
-                    defaultValue={prevHash}
-                    disabled={true}
-                />
-                
-            </div>
-            <div className="mb-4">
-                <label
-                    className="text-gray-800"
-                    htmlFor="hash"
-                >Hash:</label>
-     
-                <input 
-                    id="hash"
-                    type="text"
-                    className="mt-2 block w-full p-3 bg-gray-50"
-                    placeholder="hash"
-                    name="hash"
-                    defaultValue={hash}
-                    disabled={true}
-                />
-            </div>
-            <div className="mb-4">
-                <label
-                    className="text-gray-800"
-                    htmlFor="attenpt"
-                >Attempt:</label>
-     
-                <input 
-                    id="attempt"
-                    type="number"
-                    className="mt-2 block w-full p-3 bg-gray-50"
-                    placeholder="attempt"
-                    name="attempt"
-                    defaultValue={attempt}
-                    disabled={true}
-                />
-            </div>
+
         </div>
-            
-        </div>
-  )
+    )
 }
 
 export default Block
--- src/pages/Blockchain.tsx
+++ src/pages/Blockchain.tsx
@@ -3,7 +3,7 @@ import Block from "../components/Block"
 
 
 interface IBlock {
-    number?: number,
+    height?: number,
     nonce?: number,
     data?: string,
     prevHash?: string,
@@ -12,46 +12,60 @@ interface IBlock {
 
 const Blockchain = () => {
     const [blocks, setBlocks] = useState<Array<IBlock>>([]);
+    const [blocksLength, setBlockLength] = useState<number>(0);
+    
 
     useEffect(() => {
         const setData = () => {
             const block1: IBlock = {
-                number: 1,
+                height: 0,
                 nonce: 1,
                 data: "data1",
                 prevHash: "1234",
                 hash: "12345"
             }
             const block2: IBlock = {
-                number: 2,
+                height: 1,
                 nonce: 2,
                 data: "data2",
                 prevHash: "1111234",
                 hash: "12343335"
             }
-            setBlocks([...blocks, block1, block2]);
-
+           setBlocks([block1, block2]); 
         }
         setData();
-
-
     }, [])
+
+    useEffect(()=>{
+        const _blockLength = ()=>{
+            setBlockLength(blocks.length);
+        }
+        _blockLength();
+    },[blocks])
+    console.log("blocksLength", blocks.length);
     return (
         <>
-            <div>Blockchain Demo</div>
+            <h1 className="font-bold text-center text-4xl">Blockchain Demo</h1>
+            <p className="text-center">This site emulates a Proof-of-Work Blockchain</p>
             {blocks.map(blockMap => (
 
                 <Block
-                    key={blockMap.number}
-                    number={blockMap.number}
+                    key={blockMap.height}
+                    height={blockMap.height}
                     nonce={blockMap.nonce}
                     data={blockMap.data}
                     prevHash={blockMap.prevHash}
                     hash={blockMap.hash}
                 />
             ))}
-            <Block 
-                number={blocks.length}    
+
+            <Block
+                height={blocksLength}
+                key={blocksLength}
+                nonce={0}
+                data={""}
+                prevHash={""}
+                hash={""}                    
             />
         </>
     )
